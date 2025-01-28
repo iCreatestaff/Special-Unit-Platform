cd1503ff1b6bdef06b01c61672b71502ff05fda8
mostfajd <mohamedmostfa.jadoui6157@istic.ucar.tn>
2025-01-28 16:19:09 +0100

    equipment and subequipment fully working

--- DTO/SubEquipmentDTO.cs
+++ DTO/SubEquipmentDTO.cs
@@ -3,6 +3,9 @@ namespace WeatherApi.DTOs
     public class SubEquipmentDto
     {
         public int Id { get; set; }
-        
+        public string? Name { get; set; }
+        public string? Cycle { get; set; }
+        public bool? Status { get; set; }
+        public int? EquipmentId { get; set; } // The foreign key
     }
 }
