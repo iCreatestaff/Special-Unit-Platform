5d809bfc74bc8f6335c0d8fb9dc8f03826cbff7d
iCreatestaff <icreate.staff@gmail.com>
2025-03-08 07:57:52 +0100

    trainingss

--- obj/Debug/net8.0/sp_back.AssemblyInfo.cs
+++ obj/Debug/net8.0/sp_back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sp_back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a4a27c420f9c1e2bb40461a7f1f8063c8aae72e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b650c405528389aae823078dc455ab6b66c03a62")]
 [assembly: System.Reflection.AssemblyProductAttribute("sp_back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sp_back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
